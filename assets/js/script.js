ae11380e74ff9534589e0b933941fdc955757908
Jennifer Mitzman <jenmitz@gmail.com>
2020-11-15 19:00:29 -0800

    we did the thing

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,26 +14,36 @@
 // $.ajax(settings).done(function (response) {
 // 	console.log(response);
 // });
+console.log("haha");
 
-$("#form").submit(function (event) {
+let main = $("#form").submit(function (event) {
 	let userInput = $("#textarea1").val();
 	console.log("input" + userInput);
 	const settings = {
 		"async": true,
 		"crossDomain": true,
-		"url": "https://edamam-recipe-search.p.rapidapi.com/search?q=" + userInput + "&diet=" + "balanced",
+		"url": "https://edamam-recipe-search.p.rapidapi.com/search?q=" + userInput,
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-key": "9d3fc1b0e6mshf2a5aae79eb7bb6p179335jsnf7cabbb320b3",
 			"x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com"
 		}
-	};
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-	});
-
-	event.preventDefault();
+    };
+    
+    event.preventDefault();
+    
+    hello.forEach(element => {
+        $.ajax(settings).then(function (response) {
+            const test = JSON.stringify(response.hits.recipe.url);
+            let hello = response.hits;
+            // print out to this
+            // document.getElementById("results").textContent = JSON.stringify(response.hits);
+            document.getElementById("recipes").textContent = test;
+            console.log(response.hits)
+        });
+    });
 });
+
 
 
 
